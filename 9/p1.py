c56f7795c834b28852d3c72dafd68ad46b18d429
piggypiggyyoinkyoink <quackquack904@gmail.com>
2024-12-09 14:54:45 +0000

    day 9 part 1 done

---
+++ 9/p1.py
@@ -0,0 +1,41 @@
+f = open("9/input.txt", "r")
+li = list(f.readline().strip())
+li = list("2333133121414131402")
+driveMap = []
+idnum = 0
+for i in range(len(li)):
+    if i % 2 == 0:
+        for j in range(int(li[i])):
+            driveMap.append(idnum)
+        idnum += 1
+    else:
+        for j in range(int(li[i])):
+            driveMap.append(".")
+
+import re
+print(len(driveMap))
+j = 0
+while re.fullmatch(r"[0-9]*\.*", "".join(str(x) for x in driveMap)) == None:
+        while j < len(driveMap) and driveMap[j]!= ".":
+            j+=1
+        if j == len(driveMap):
+            break
+        if driveMap[j] == ".":
+            for i in range(len(driveMap)-1, -1, -1):
+                if driveMap[i] != ".":
+                    break
+            driveMap[j] = driveMap[i]
+            driveMap[i] = "."
+        
+
+
+print("hello")
+print(driveMap)
+checksum = 0
+for i in range(len(driveMap)):
+    if driveMap[i] == ".":
+        break
+    else:
+        checksum += driveMap[i] * i
+
+print(checksum)
